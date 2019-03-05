0aa6647131a3e13a74c8ad841e1964d25259b941
lucien1011 <lucien1011@gmail.com>
2019-03-05 10:06:58 -0500

    Modified BBF minutes

--- email_BBF.sh
+++ email_BBF.sh
@@ -3,4 +3,5 @@
 #python email_BBF_meeting.py "17h tomorrow" 42-R-407 https://indico.cern.ch/event/802744/
 #python email_BBF_minutes.py https://indico.cern.ch/event/802744/
 
-python email_BBF_meeting.py "15h tomorrow" 42-R-407 https://indico.cern.ch/event/803151/
+#python email_BBF_meeting.py "15h tomorrow" 42-R-407 https://indico.cern.ch/event/803151/
+python email_BBF_minutes.py "05-03-2019" https://indico.cern.ch/event/803151/
--- email_BBF_minutes.py
+++ email_BBF_minutes.py
@@ -1,8 +1,15 @@
 from Emailer.Utils import sendQuickMail
 import sys
 
+import datetime
+
+date_format = "%d-%m-%Y"
+
+meeting_date_str = sys.argv[1]
+meeting_date = datetime.datetime.strptime(meeting_date_str, date_format).date()
+
 sendQuickMail(
         "BBF-HZZ-Analysis@cern.ch",
-        "Minutes for BBF meeting",
-        "Hi guys,\n\nPlease find the meeting minutes here: {link}note/.\n\nKind regards,\nLucien".format(link=sys.argv[1]),
+        "Minutes for BBF meeting: "+meeting_date.strftime("%B %d, %Y"),
+        "Hi guys,\n\nPlease find the meeting minutes here: {link}note/.\n\nKind regards,\nLucien".format(link=sys.argv[2]),
         )
